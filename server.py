136f4214b6ec2c4f3a4bf653f563d0b179014e58
shenmo3 <ahliu@umail.ucsb.edu>
2020-03-08 00:45:22 -0800

    error handling

--- server.py
+++ server.py
@@ -20,31 +20,39 @@ class Server(Thread):
         self.setting = setting
 
     def run(self):
-        delay = True
-        while True:
-            if self.shut:
-                break
-            if self.setting.server_msg:
-                msg = self.setting.server_msg.encode("ISO-8859-1")
-                self.setting.server_msg = ""
-                self.client.sendall(msg)
-                print("proxy -> server: ", msg)
-            r, w, e = select.select((self.server,), (), (), 0)
-            if r:
-                data = self.server.recv(4096)
-                importlib.reload(parser)
-                data = parser.server_parser(data)
-                if data:
-                    # delay
-                    if delay:
-                        time.sleep(self.setting.delay + random.gauss(self.setting.jitter[0], self.setting.jitter[1]))
-                        delay = False
-                    # loss
-                    if random.random() > self.setting.loss:
-                        self.client.sendall(data)
-                else:
-                    self.event.set()
+        try:
+            delay = True
+            while True:
+                if self.shut:
+                    self.server.close()
                     break
-            else:
-                delay = True
-        print("server closed")
+                if self.setting.server_msg:
+                    msg = self.setting.server_msg.encode("ISO-8859-1")
+                    self.setting.server_msg = ""
+                    self.client.sendall(msg)
+                    print("proxy -> server: ", msg)
+                r, w, e = select.select((self.server,), (), (), 0)
+                if r:
+                    data = self.server.recv(4096)
+                    try:
+                        importlib.reload(parser)
+                        data = parser.server_parser(data, self.port)
+                    except Exception as e:
+                        print("server parser error", self.port, e)
+                    if data:
+                        # delay
+                        if delay:
+                            time.sleep(self.setting.delay + random.gauss(self.setting.jitter[0], self.setting.jitter[1]))
+                            delay = False
+                        # loss
+                        if random.random() > self.setting.loss:
+                            self.client.sendall(data)
+                    else:
+                        self.event.set()
+                        break
+                else:
+                    delay = True
+            print("server closed")
+        except Exception as e:
+            print("server", self.port, e)
+            self.event.set()
